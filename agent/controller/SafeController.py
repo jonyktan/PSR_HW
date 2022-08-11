6765c5724785c007fa475ce3eba2f4eabfc7739d
jonyktan <jon.yktan03@gmail.com>
2022-08-11 17:15:36 +0800

    Implemented QP optimiser

--- agent/controller/SafeController.py
+++ agent/controller/SafeController.py
@@ -6,6 +6,8 @@ import numpy as np
 from abc import ABC, abstractmethod
 from utils import GoalType
 import cvxopt
+from cvxopt import matrix
+from cvxopt import solvers
 
 class SafeController(ABC):
     def __init__(self, spec, model):
@@ -63,14 +65,20 @@ class EnergyFunctionController(SafeController):
         # raise NotImplementedError # TODO delete this line
 
         # TODO compute the following terms
-        c = 1 # c is some conservativeness factor, c > 0
-        k = 1 # k is some scaling factor for dot(phi_0), k > 0
+        c = self._spec['c'] # c is some conservativeness factor, c > 0
+        k = self.k_v # k is some scaling factor for dot(phi_0), k > 0
         d_min = self.d_min
-        phi_0       = d_min - np.sqrt((ce[0][0]-co[0][0])^2 + (ce[0][1]-co[0][1])^2)
-        dot_phi_0   = - np.sqrt((ce[0][2]-co[0][2])^2 + (ce[0][3]-co[0][3])^2)
-        phi         = c + phi_0 + k * dot_phi_0 
+        x_rel   = ce[0][0] - co[0][0]
+        y_rel   = ce[1][0] - co[1][0]
+        dot_x_rel   = ce[2][0] - co[2][0]
+        dot_y_rel   = ce[3][0] - co[3][0]
+        # print(f'x_rel = {x_rel}')
+        # print(f'y_rel = {y_rel}')
+        d   = np.sqrt(x_rel**2 + y_rel**2)
+        dot_d       = (x_rel*dot_x_rel + y_rel * dot_y_rel)/d
+        phi         = c + d_min**2 - d**2 - k * dot_d 
         p_phi_p_ce  = 0 # not used 
-        p_phi_p_co  = 0 # not used
+        p_phi_p_co  = 0 # not used 
         
         return phi, p_phi_p_ce, p_phi_p_co
     
@@ -127,8 +135,23 @@ class SafeSetController(EnergyFunctionController):
         # raise NotImplementedError # TODO delete this line
 
         # TODO compute the following terms
-        phi = self.phi_and_derivatives(dt, ce, co)[0]
-        u = None
+        phi, _, _ = self.phi_and_derivatives(dt, ce, co)
+
+        # Compute the following terms for QP
+        x_rel   = ce[0][0] - co[0][0]
+        y_rel   = ce[1][0] - co[1][0]
+        dot_x_rel   = ce[2][0] - co[2][0]
+        dot_y_rel   = ce[3][0] - co[3][0]
+
+        # CVXOPT QP solver in the form of min 1/2 * u^T * P * u + q^T * u, s.t. G * u <= h
+        P = matrix(np.array([[2,0],[0,2]]), tc='d')
+        q = matrix(np.array([-2*u_ref[0], -2*u_ref[1]]), tc='d')
+        # G = matrix(np.array([[-(self.k_v*dot_x_rel)/(np.sqrt(x_rel**2 + y_rel**2)), -(self.k_v*dot_y_rel)/(np.sqrt(x_rel**2 + y_rel**2))], [1,0], [0,1], [-1,0], [0,-1]]), tc='d')
+        # h = matrix(np.array([-self.eta + 2*(x_rel*dot_x_rel + y_rel*dot_y_rel) - self.k_v*(x_rel*dot_x_rel + y_rel*dot_y_rel)**2/(x_rel**2 + y_rel**2)**(3/2), 200, 200, 200, 200]), tc='d')
+        G = matrix(np.array([[-(self.k_v*dot_x_rel)/(np.sqrt(x_rel**2 + y_rel**2)), -(self.k_v*dot_y_rel)/(np.sqrt(x_rel**2 + y_rel**2))]]), tc='d')
+        h = matrix(np.array([-self.eta + 2*(x_rel*dot_x_rel + y_rel*dot_y_rel) - self.k_v*(x_rel*dot_x_rel + y_rel*dot_y_rel)**2/(x_rel**2 + y_rel**2)**(3/2)]), tc='d')
+        sol = solvers.qp(P,q,G,h)
+        u = sol['x']
 
         return phi, u
 
